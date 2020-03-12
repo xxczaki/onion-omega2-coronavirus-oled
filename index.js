0eac3d9a69a24ddb7f451490b91c232a795bc607
Antoni Kepinski <xxczaki@pm.me>
2020-03-12 14:21:54 +0100

    wrap everything in setInterval

--- index.js
+++ index.js
@@ -14,37 +14,39 @@ const query = `{
 }`;
 
 (async () => {
-	try {
-		let stats = {
-			confirmed: undefined,
-			recovered: undefined,
-			deaths: undefined
-		};
-
-		const response = await graphqlGot('https://coronavirusapi.me/', {query});
-		const data = response.body.locations.filter(object => object.region === region);
-
-		// eslint-disable-next-line array-callback-return
-		await data.map(element => {
-			stats = {
-				confirmed: element.confirmed,
-				recovered: element.recovered,
-				deaths: element.deaths
+	setInterval(() => {
+		try {
+			let stats = {
+				confirmed: undefined,
+				recovered: undefined,
+				deaths: undefined
 			};
-		});
-
-		omegaOled.init().then(() => {
-			omegaOled.chainMode(true);
-			omegaOled.write(`Status for ${region}:`);
-			omegaOled.cursor(2, 0);
-			omegaOled.write(`Confirmed: ${stats.confirmed}`);
-			omegaOled.cursor(3, 0);
-			omegaOled.write(`Recovered: ${stats.recovered}`);
-			omegaOled.cursor(4, 0);
-			omegaOled.write(`Deaths: ${stats.deaths}`);
-			omegaOled.executeChain();
-		});
-	} catch (error) {
-		console.log(error.response.body);
-	}
+	
+			const response = await graphqlGot('https://coronavirusapi.me/', {query});
+			const data = response.body.locations.filter(object => object.region === region);
+	
+			// eslint-disable-next-line array-callback-return
+			await data.map(element => {
+				stats = {
+					confirmed: element.confirmed,
+					recovered: element.recovered,
+					deaths: element.deaths
+				};
+			});
+	
+			omegaOled.init().then(() => {
+				omegaOled.chainMode(true);
+				omegaOled.write(`Status for ${region}:`);
+				omegaOled.cursor(2, 0);
+				omegaOled.write(`Confirmed: ${stats.confirmed}`);
+				omegaOled.cursor(3, 0);
+				omegaOled.write(`Recovered: ${stats.recovered}`);
+				omegaOled.cursor(4, 0);
+				omegaOled.write(`Deaths: ${stats.deaths}`);
+				omegaOled.executeChain();
+			});
+		} catch (error) {
+			console.log(error.response.body);
+		}
+	}, 300000);
 })();
